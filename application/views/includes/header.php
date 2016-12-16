b047d97963e2747169070d9d17b971222a423f20
thegevorgyan <g.artak@yahoo.com>
2016-12-16 17:42:13 +0400

    commit 16.12

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -10,9 +10,9 @@
     <script src="<?=base_url();?>/public/js/vendor/angular.min.js"></script>
     <script src="<?=base_url();?>/public/js/vendor/angular-ui-router.min.js"></script>
     <!--script src="https://cdnjs.cloudflare.com/ajax/libs/angular-touch/1.5.7/angular-touch.min.js"></script-->
+    <script src="<?=base_url();?>/public/js/vendor/bootstrap.min.js"></script>
     <script src="<?=base_url();?>/public/js/vendor/ui-bootstrap.min.js"></script>
     <script src="<?=base_url();?>/public/js/vendor/ui-bootstrap-tpls.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 </head>
 <body>
     <header ng-controller='headerCtrl'>
@@ -25,45 +25,29 @@
     ?>
 
     </div>
-
-        <div>
-<div class='col-sx-3 col-sm-3 col-md-3 col-lg-3'>
-        <a href="<?=base_url() . $this->uri->segment(1)?>"><div class="logo"></div></a>
-</div>
-
-
-<div class='col-sx-3 col-sm-3 col-md-3 col-lg-3'>
-        <?php 
-
-            if(!$this->session->userdata('usr')){
-                echo anchor($this->session->userdata('lang'). "/login" , lang('signin'), 'id="singin" class="btn lp_link"');
-            }else{
-                echo anchor($this->session->userdata('lang'). "/login/logout" , lang('logout'), 'class="btn lp_link"');
-            }
-            echo anchor($this->session->userdata('lang'). "/login/signup" , lang('signup'), 'class="btn lp_link"');
-        ?>
-</div>
-
-<div class='col-sx-6 col-sm-6 col-md-6 col-lg-6' style='position:absolute; top:20px;right:50px'>
-        <?php 
-           // echo heading('This is header page', 3);
-          //  echo $this->uri->total_rsegments(). br();
-           // echo br() . $this->uri->ruri_string() . br();
-            //echo session_id() . "<br>";
-            
-         //   echo $this->session->userdata('lang'). "<br>";
-            
-
-            if($this->uri->total_rsegments() > 2){
-              $url = explode('/', $this->uri->ruri_string(), $this->uri->total_rsegments());
-            }  
-
-            echo "<a href='".base_url()."en/".$this->uri->ruri_string()."' id='lang_en'><div id='flag_en' class='pull-right flags ".$this->uri->segment(1)."' title='".lang('english')."'></div></a>";
-            echo "<a href='".base_url()."ru/".$this->uri->ruri_string()."' id='lang_ru'><div id='flag_ru' class='pull-right flags ".$this->uri->segment(1)."' title='".lang('russian')."'></div></a>";
-            echo "<a href='".base_url()."am/".$this->uri->ruri_string()."' id='lang_am'><div id='flag_am' class='pull-right flags ".$this->uri->segment(1)."' title='".lang('armenian')."'></div></a>";
-            
-            ?>
-</div>            
-
-</div>
+    <div>
+        <div class='col-sx-3 col-sm-3 col-md-3 col-lg-3'>
+                <a href="<?=base_url() . $this->uri->segment(1)?>"><div class="logo"></div></a>
+        </div>
+        <div class='col-sx-3 col-sm-3 col-md-3 col-lg-3'>
+                <?php 
+                    if(!$this->session->userdata('usr')){
+                        echo anchor($this->session->userdata('lang'). "/login" , lang('signin'), 'id="singin" class="btn lp_link"');
+                    }else{
+                        echo anchor($this->session->userdata('lang'). "/login/logout" , lang('logout'), 'class="btn lp_link"');
+                    }
+                    echo anchor($this->session->userdata('lang'). "/login/signup" , lang('signup'), 'class="btn lp_link"');
+                ?>
+        </div>
+        <div class='col-sx-6 col-sm-6 col-md-6 col-lg-6' style='position:absolute; top:20px;right:50px'>
+                <?php 
+                    if($this->uri->total_rsegments() > 2){
+                      $url = explode('/', $this->uri->ruri_string(), $this->uri->total_rsegments());
+                    }  
+                    echo "<a href='".base_url()."en/".$this->uri->ruri_string()."' id='lang_en'><div id='flag_en' class='pull-right flags ".$this->uri->segment(1)."' title='".lang('english')."'></div></a>";
+                    echo "<a href='".base_url()."ru/".$this->uri->ruri_string()."' id='lang_ru'><div id='flag_ru' class='pull-right flags ".$this->uri->segment(1)."' title='".lang('russian')."'></div></a>";
+                    echo "<a href='".base_url()."am/".$this->uri->ruri_string()."' id='lang_am'><div id='flag_am' class='pull-right flags ".$this->uri->segment(1)."' title='".lang('armenian')."'></div></a>";
+                ?>
+        </div>
+    </div>
     </header>    
