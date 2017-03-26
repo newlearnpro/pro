7c7786ade977a95d3e62f3f7450ba242d4d3697e
thegevorgyan <g.artak@yahoo.com>
2017-03-26 12:40:32 +0400

    commit 26.03

---
+++ uploads/lesson_type_5/05159_1490439041/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "61I6GmlOJXY":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  parent.document.getElementsByClassName("lesson_selected")[0].parentElement.nextElementSibling.firstElementChild.click();
+}
+
